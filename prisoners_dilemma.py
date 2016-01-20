2b575e7a78d87d55df4c07c7709c165045166303
The Bear Mayor <jorrobinson830@gmail.com>
2016-01-20 09:49:52 -0600

    Team 7
    Added team 7's code, Kade May and Jordon Robinson

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -276,16 +276,19 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 7:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'Honey Bear'
         else:
             # use history, opponent_history, score, opponent_score
             # to compute your strategy
             if len(opponent_history)==0: #It's the first round: collude
                 return 'c'
             elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were sucker last time
+                return 'b' # betray is they were severely punished last time
+            elif history[-1]=='b' and opponent_history[-1]=='b':
+                return 'b' # betray is they were punished last time
             else:
                 return 'c' #otherwise collude
+
 
 
 
